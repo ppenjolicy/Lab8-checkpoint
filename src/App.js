41fe0786d246533503ac02768ad6ff86b88748e9
ppenjolicy <yurin.pk.123@gmail.com>
2020-03-19 19:36:51 +0700

    todo list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { firestore } from './index'
+import Task from './component/Task'
 
 function App() {
+
+  const [tasks, setTasks] = useState([])
+
+  const [name, setName] = useState('')
+
+  useEffect(() => {
+    retriveData()
+  }, [])
+
+  const retriveData = () => {
+    firestore.collection("tasks").onSnapshot((snapshot) => {
+      console.log(snapshot.docs)
+      let mTask = snapshot.docs.map(d => {
+        const { id, name } = d.data()
+        console.log(id, name)
+        return { id, name }
+      })
+      setTasks(mTask)
+    })
+  }
+
+  const deleteTask = (id) => {
+    firestore.collection("tasks").doc(id + '').delete()
+  }
+
+  const editTask = (id) => {
+    firestore.collection("tasks").doc(id + '').set({ id, name })
+  }
+
+
+  const renderTask = () => {
+    if (tasks && tasks.length)
+      return tasks.map((task, index) => {
+        return (<Task 
+          key={index}
+          task={task}
+          editTask={editTask}
+          deleteTask={deleteTask} />)
+      })
+    else
+      return (<li> No task </li>)
+
+  }
+
+  const addTask = () => {
+    let id = (tasks.length === 0) ? 1 : tasks[tasks.length - 1].id + 1
+    firestore.collection("tasks").doc(id + '').set({ id, name })
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo</h1>
+      <input type='text' name='name' onChange={(e) => { setName(e.target.value) }}></input>
+      <button onClick={addTask}>Submit </button> <br />
+      <ul style={{ display: 'flex', listStyle: 'none' }}> {renderTask()}</ul>
     </div>
   );
 }
---
+++ src/config.js
@@ -0,0 +1,13 @@
+
+const config = {
+    apiKey: "AIzaSyBPKsaBlPy4N7inzU62OdwcaVHE2Q24q08",
+    authDomain: "todo-44ca2.firebaseapp.com",
+    databaseURL: "https://todo-44ca2.firebaseio.com",
+    projectId: "todo-44ca2",
+    storageBucket: "todo-44ca2.appspot.com",
+    messagingSenderId: "544549515946",
+    appId: "1:544549515946:web:cfbaa9174d520c4aa796d3",
+    measurementId: "G-5288CGTSTV"
+  };
+
+  export default config
